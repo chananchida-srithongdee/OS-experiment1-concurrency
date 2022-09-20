362cc8588c1f3993f2b627c6f85f6335ad3e0854
chananchida-srithongdee <63010187@kmitl.ac.th>
2022-09-20 13:52:04 +0700

    first commit

---
+++ Experiment3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+// experiment3 : pause a thread
+namespace Lab_OS_Concurrency02
+{
+    class Program
+    {
+        static int resource = 10000;
+        static void TestThread1()
+        {
+            resource = 55555;
+            // Console.WriteLine("Done");
+
+        }
+        static void Main(string[] args)
+        {
+            Thread th1 = new Thread(TestThread1);
+            th1.Start();
+            Thread.Sleep(100);
+            Console.WriteLine("resource={0}", resource);
+        }
+    }
+}
